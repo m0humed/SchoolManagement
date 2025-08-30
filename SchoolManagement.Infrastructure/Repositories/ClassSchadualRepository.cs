a3744d72184ac4de2e8a67599d1ad3f9723e487e
m0humed <qandilmohammed83@gmail.com>
2025-08-30 07:07:55 +0300

    Add ClassSchadual Repository

---
+++ SchoolManagement.Infrastructure/Repositories/ClassSchadualRepository.cs
@@ -0,0 +1,37 @@
+﻿using SchoolManagement.Infrastructure.IRepositories;
+using Schoolmanagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagement.Infrastructure.Repositories
+{
+    public class ClassSchadualRepository : GenaricRepository<ClassSchadual, Guid>, IClassSchadual
+    {
+        #region Fields
+        private readonly ApplicationDbContext _context;
+        #endregion
+        public ClassSchadualRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ClassSchadual>> GetSchadualByClassIdAsync(Guid classId)
+        {
+            var schaduals = _context.classSchaduals.Where(cs => cs.ClassId == classId);
+            var orderSchadual = await schaduals.OrderBy(x=>x.DayOfWeek).ThenBy(x=>x.StartTime).ToListAsync();
+            return orderSchadual;
+        }
+
+        public async Task<IEnumerable<ClassSchadual>> GetSchadualByTeacherIdAsync(string teacherId)
+        {
+            var schadual = _context.classSchaduals.Where(cs=>cs.TeacherId.Equals(teacherId));
+            var orderSchadual = await schadual.OrderBy(x => x.DayOfWeek).ThenBy(x => x.StartTime).ToListAsync();
+            return orderSchadual;
+        }
+    }
+}
