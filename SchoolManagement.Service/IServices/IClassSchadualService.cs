525914df35ad1e9cefaa0eb740fc0d9fb29690a0
m0humed <qandilmohammed83@gmail.com>
2025-08-30 08:14:31 +0300

    Add CLassSchadual Service

---
+++ SchoolManagement.Service/IServices/IClassSchadualService.cs
@@ -0,0 +1,16 @@
+﻿using Schoolmanagement.Domain.Entities;
+using SchoolManagement.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Service.IServices
+{
+    public interface IClassSchadualService:IService<ClassService , Guid>
+    {
+        Task<IEnumerable<ClassSchadual>> GetSchadualByClassIdAsync(Guid classId);
+        Task<IEnumerable<ClassSchadual>> GetSchadualByTeacherIdAsync(string teacherId);
+    }
+}
---
+++ SchoolManagement.Service/Services/ClassSchadualService.cs
@@ -0,0 +1,61 @@
+﻿using Schoolmanagement.Domain.Entities;
+using SchoolManagement.Infrastructure.IRepositories;
+using SchoolManagement.Service.IServices;
+
+
+namespace SchoolManagement.Service.Services
+{
+    public class ClassSchadualService : IClassSchadualService
+    {
+        #region Field
+        private IClassSchadualRepository _classSchadualRepository;
+        #endregion
+
+        #region Constructors
+        public ClassSchadualService(IClassSchadualRepository classSchadualRepository)
+        {
+            _classSchadualRepository = classSchadualRepository;
+        }
+        #endregion
+
+        public async Task AddAsync(ClassSchadual entity)
+        {
+           await _classSchadualRepository.AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _classSchadualRepository.DeleteAsync(id);
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            return await _classSchadualRepository.ExistsAsync(id);
+        }
+
+        public async Task<IEnumerable<ClassSchadual>> GetAllAsync()
+        {
+             return await _classSchadualRepository.GetAllAsync();
+        }
+
+        public Task<ClassSchadual> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ClassSchadual>> GetSchadualByClassIdAsync(Guid classId)
+        {
+            return await _classSchadualRepository.GetSchadualByClassIdAsync(classId);
+        }
+
+        public async Task<IEnumerable<ClassSchadual>> GetSchadualByTeacherIdAsync(string teacherId)
+        {
+            return await _classSchadualRepository.GetSchadualByTeacherIdAsync(teacherId);
+        }
+
+        public Task UpdateAsync(ClassSchadual entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
