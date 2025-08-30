02ffdc69497ed72417861f99da5123b7fa6eb83a
m0humed <qandilmohammed83@gmail.com>
2025-08-30 07:25:39 +0300

    Update Class Schadual

---
+++ SchoolManagement.Infrastructure/IRepositories/IClassSchadualRepository.cs
@@ -0,0 +1,16 @@
+﻿using Schoolmanagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Infrastructure.IRepositories
+{
+    public interface IClassSchadualRepository:IRepository<ClassSchadual,Guid>
+    {
+        Task<IEnumerable<ClassSchadual>> GetSchadualByClassIdAsync(Guid classId);
+        Task<IEnumerable<ClassSchadual>> GetSchadualByTeacherIdAsync(string teacherId);
+
+    }
+}
